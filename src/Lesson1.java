9af73895ac31501a0a9aa12fb988c59ecd54f974
sezerfurkankirpitci <sezer.furkan.kirpitci@gmail.com>
2023-04-24 18:31:22 +0300

    commit

---
+++ src/Lesson1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Lesson1 {
+
+	public static void main(String[] args) {
+		  Scanner scan = new Scanner(System.in);
+	        int birinciNot;
+	        int ikinciNot;
+	        float ortalama;
+	        
+	        String ilkMesaj = "Hoşgeldiz.";
+	        String birinciNotMesaji = "Lütfen 1. notunuzu giriniz : ";
+	        String ikinciNotMesaji = "Lütfen 2. notunuzu giriniz : ";
+	        String ortalamaMesaji = "Ortalamanız = ";
+	        
+	        System.out.println(ilkMesaj);
+	        System.out.println("Merhaba Dünya!");
+	        
+	        System.out.println(birinciNotMesaji);
+	        birinciNot = scan.nextInt();
+	        
+	        System.out.println(ikinciNotMesaji);
+	        ikinciNot = scan.nextInt();
+	        
+	        ortalama = (birinciNot + ikinciNot) / 2 ;
+	        System.out.println(ortalamaMesaji + ortalama);
+	}
+
+}
